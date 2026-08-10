a2d8b44ea3f52a06aa7aa4f3373827656db7a54b
Aryan Sharma <90350395+realaryansharma@users.noreply.github.com>
2026-08-10 14:33:15 +0530

    Pattern 2 completed

---
+++ patterns/PatternTwo.java
@@ -0,0 +1,29 @@
+package patterns;
+
+import java.util.*;
+
+public class PatternTwo {
+    
+    public static void main(String args[]) {
+
+        Scanner input = new Scanner(System.in);
+
+        int rows;
+
+        rows = input.nextInt();
+
+        for(int i=1; i<rows; i++) {
+
+            for(int j=1; j<=i; j++) {
+
+                System.out.print("*");
+
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+}
