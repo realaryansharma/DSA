031f603778acdfd4cc2d7e271dd33f98df4b791e
Aryan Sharma <90350395+realaryansharma@users.noreply.github.com>
2026-08-10 20:07:47 +0530

    Completed Pattern seven

---
+++ patterns/PatternSeven.java
@@ -0,0 +1,36 @@
+package patterns;
+
+import java.util.*;
+
+public class PatternSeven {
+    
+    public static void main(String args[]) {
+
+        Scanner input = new Scanner(System.in);
+
+        int rows;
+
+        rows = input.nextInt();
+
+        for(int i=1; i<=rows; i++) {
+
+            for(int j=1; j<=rows-i; j++) {
+
+                System.out.print(" ");
+
+            }
+
+            for(int j=1; j<=(2*i)-1; j++) {
+
+                System.out.print("*");
+
+            }
+
+            System.out.println();
+
+        }
+
+
+    }
+
+}
