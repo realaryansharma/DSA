9f12543b03e4a9f1f56ddf59f5e7613d658fc6cc
Aryan Sharma <90350395+realaryansharma@users.noreply.github.com>
2026-08-11 14:13:39 +0530

    Completed Pattern Nine Ten Eleven

---
+++ patterns/PatternEleven.java
@@ -0,0 +1,34 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class PatternEleven {
+
+    public static void main(String args[]) {
+
+
+        Scanner input = new Scanner(System.in);
+
+        int n;
+
+        n = input.nextInt();
+
+        int start;
+
+        for(int i=1; i<=n; i++) {
+
+            start = i % 2;
+
+            for(int j=1; j<=i; j++) {
+
+                System.out.print(start + " ");
+                start = 1-start;
+
+            }
+
+            System.out.println();
+
+        } 
+    }
+    
+}
