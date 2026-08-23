1e81006b08fc9f118f004c6e841206e500406e74
Aryan Sharma <90350395+realaryansharma@users.noreply.github.com>
2026-08-23 13:56:49 +0530

    Largest ODd Number string question fixed, working now

--- basic/strings/LargestOddNumberInString.java
+++ basic/strings/LargestOddNumberInString.java
@@ -1,31 +1,23 @@
-package basic.strings;
-
-public class LargestOddNumberInString {
+class Solution {    
     public String largeOddNum(String s) {
-        //your code goes here
-        long checkNum, max=-1;
+        int end = s.length() - 1;
 
-        if(s.equals("0"))
-            return "";
-
-        //multi loop 0 to length-1
-        //loop i to length -1, get substring and check if odd, then update max number if needed
-
-        for(int i=0; i<s.length(); i++) {
-
-            for(int j=i+1; j<=s.length(); j++) {
-
-                checkNum = Integer.parseInt(s.substring(i, j));
-
-                if(checkNum%2 != 0 && checkNum > max) {
-                    max = checkNum;
-                }
-
-            }
-
+        // 1. Find the rightmost odd digit
+        while (end >= 0 && (s.charAt(end) - '0') % 2 == 0) {
+            end--;
         }
 
-        return String.valueOf(max);
+        // If no odd digit exists in the entire string
+        if (end < 0) {
+            return "";
+        }
 
+        // 2. Skip leading zeros
+        int start = 0;
+        while (start <= end && s.charAt(start) == '0') {
+            start++;
+        }
+
+        return s.substring(start, end + 1);
     }
 }
