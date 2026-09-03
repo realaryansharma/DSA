4ff72fb46a44f7292e42fae6a550594df7f336e4
Aryan Sharma <90350395+realaryansharma@users.noreply.github.com>
2026-09-03 13:51:50 +0530

    Completed Majority Element one

---
+++ advance/arrays/medium/MajorityElementOne.java
@@ -0,0 +1,21 @@
+package advance.arrays.medium;
+
+import java.util.HashMap;
+
+public class MajorityElementOne {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+        int threshold = nums.length / 2;
+
+        for (int num : nums) {
+            int count = frequencyMap.getOrDefault(num, 0) + 1;
+            frequencyMap.put(num, count);
+
+            if (count > threshold) {
+                return num;
+            }
+        }
+
+        return -1;
+    }    
+}
