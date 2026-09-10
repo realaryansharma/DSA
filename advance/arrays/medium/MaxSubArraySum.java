8a3bb727db9b20fa6b4db4fdecca5e5ff28b352c
Aryan Sharma <90350395+realaryansharma@users.noreply.github.com>
2026-09-10 08:29:34 +0530

    Completed optimmised max sum array sum Kadane algorithm approach

--- advance/arrays/medium/MaxSubArraySum.java
+++ advance/arrays/medium/MaxSubArraySum.java
@@ -26,4 +26,21 @@ public class MaxSubArraySum {
 
     }
 
+    public int maxSubArrayOptimisedApproach(int[] nums) {
+        
+        int sum=0, maxSum = nums[0];
+
+        for(int i=0; i<nums.length; i++) {
+
+            sum+=nums[i];
+            
+            if(sum<0)
+                sum=0;
+
+        }
+
+        return sum;
+
+    }
+
 }
