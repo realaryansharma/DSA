59d69c7d3318ac2e3c96aad0b3a05bc2c2d28797
Aryan Sharma <90350395+realaryansharma@users.noreply.github.com>
2026-09-12 13:15:59 +0530

    Completed Majority Element Three java

---
+++ advance/arrays/hard/MajorityElementThree.java
@@ -0,0 +1,29 @@
+package advance.arrays.hard;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MajorityElementThree {
+    public List<Integer> majorityElementTwo(int[] nums) {
+        
+        int threshold = (nums.length/3) + 1;
+        HashMap <Integer, Integer> frequencyMap = new HashMap<>();
+
+        List<Integer> answer = new ArrayList<>();
+
+        for(int i=0; i<nums.length; i++) {
+
+            int count = frequencyMap.getOrDefault(nums[i], 0) + 1;
+            frequencyMap.put(nums[i], count);
+
+            if (count == threshold) {
+                answer.add(nums[i]);
+            }
+
+        }
+
+        return answer;
+
+    }
+}
