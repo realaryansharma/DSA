c620896dd14f20bab5140061bcccf6da18fa3d1e
Aryan Sharma <90350395+realaryansharma@users.noreply.github.com>
2026-09-16 07:48:36 +0530

    Completed running some of 1d array

---
+++ allPlatforms/arrays/RunningSumOfOneD.java
@@ -0,0 +1,21 @@
+package allPlatforms.arrays;
+
+public class RunningSumOfOneD {
+
+    //Leetcode: 1480
+    public int[] runningSum(int[] nums) {
+        
+        int sum=0;
+
+        for(int i=0; i<nums.length; i++) {
+
+            sum += nums[i];
+            nums[i] = sum;
+
+        }        
+
+        return nums;
+
+    }
+    
+}
