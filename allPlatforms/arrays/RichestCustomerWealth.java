dc40b21c2f08e8901e9cadb402191d3014ae476c
Aryan Sharma <90350395+realaryansharma@users.noreply.github.com>
2026-09-16 07:57:26 +0530

    Completed richest customer wealth

---
+++ allPlatforms/arrays/RichestCustomerWealth.java
@@ -0,0 +1,26 @@
+package allPlatforms.arrays;
+
+public class RichestCustomerWealth {
+    public int maximumWealth(int[][] accounts) {
+
+        int maxSum = accounts[0][0], sum=0;
+
+        for(int i=0; i<accounts.length; i++) {
+
+            sum = 0;
+
+            for(int j=0; j<accounts[i].length; j++) {
+
+                sum += accounts[i][j];
+
+            }
+
+            if(sum > maxSum)
+                maxSum = sum;
+
+        }
+
+        return maxSum;
+
+    }
+}
