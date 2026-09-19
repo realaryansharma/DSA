e3be2f662eb6693b5dd9ad260f15d7a7ebd31b19
Aryan Sharma <90350395+realaryansharma@users.noreply.github.com>
2026-09-19 07:48:20 +0530

    Count sub arrays with xor

---
+++ striverOnly/advance/hashing/CountSubArraysWithXOR.java
@@ -0,0 +1,29 @@
+package advance.hashing;
+
+import java.util.HashMap;
+
+public class CountSubArraysWithXOR {
+    public int subarraysWithXorK(int[] nums, int k) {
+        int n = nums.length;
+        int count = 0;
+        int sum = 0;
+
+        HashMap<Integer, Integer> st = new HashMap<>();
+        st.put(0, 1);
+
+        for (int i = 0; i < n; i++) {
+
+            sum ^= nums[i];
+
+            int diff = sum ^ k;
+
+            if (st.containsKey(diff)) {
+                count += st.get(diff);
+            }
+
+            st.put(sum, st.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+}
