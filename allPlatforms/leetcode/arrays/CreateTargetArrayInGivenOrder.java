69f43f4ca01c30adc921cdb6aece2268088a8a78
Aryan Sharma <90350395+realaryansharma@users.noreply.github.com>
2026-09-22 16:13:00 +0530

    Completed one leetcode

---
+++ allPlatforms/leetcode/arrays/CreateTargetArrayInGivenOrder.java
@@ -0,0 +1,20 @@
+package allPlatforms.leetcode.arrays;
+
+import java.util.ArrayList;
+
+public class CreateTargetArrayInGivenOrder {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        
+        for(int i=0;i<nums.length;i++)
+            a.add(index[i],nums[i]);
+
+        int target[] = new int[nums.length];
+        
+        for(int i=0;i<nums.length;i++)
+            target[i] = a.get(i);
+
+        return target;
+    }
+}
